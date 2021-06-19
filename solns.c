4677781724be33785e85991134b024541bbdeadf
diyalizbethjose <84697287+diyalizbethjose@users.noreply.github.com>
2021-06-19 10:59:38 +0530

    Update solns.c

--- solns.c
+++ solns.c
@@ -1,2 +1,12 @@
 /* Enter your solutions in this file */
 #include <stdio.h>
+int max (int number[1000], int n)
+{
+  int maxn=number[0];
+  for(int i=0;i<n;i++)
+  {
+    if(number[i]>maxn)
+      maxn=number[i];
+  }
+  return(maxn);
+}
